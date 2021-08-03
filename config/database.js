31daabe7b6f69b38de487042ffc81fb22d4436de
Łukasz Szarpak <lukasz.szarpak@plus.pl>
2021-08-03 15:24:55 +0200

    add content-types

--- config/database.js
+++ config/database.js
@@ -1,3 +1,28 @@
+// #### Prod
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         host: env('DATABASE_HOST'),
+//         srv: env.bool('DATABASE_SRV', true),
+//         port: env.int('DATABASE_PORT', 27017),
+//         database: env('DATABASE_NAME'),
+//         username: env('DATABASE_USERNAME'),
+//         password: env('DATABASE_PASSWORD'),
+//       },
+//       options: {
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+//         ssl: env.bool('DATABASE_SSL', true),
+//       },
+//     },
+//   },
+// });
+
+// #### Local
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
@@ -18,25 +43,3 @@ module.exports = ({ env }) => ({
     },
   },
 });
-
-
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'mongoose',
-//       settings: {
-//         host: env('DATABASE_HOST', 'strapi.kckg8.mongodb.net'),
-//         srv: env.bool('DATABASE_SRV', true),
-//         port: env.int('DATABASE_PORT', 27017),
-//         database: env('DATABASE_NAME', 'strapi'),
-//         username: env('DATABASE_USERNAME', 'Admin'),
-//         password: env('DATABASE_PASSWORD', '1Y50efRbKg24qNhB'),
-//       },
-//       options: {
-//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-//         ssl: env.bool('DATABASE_SSL', true),
-//       },
-//     },
-//   },
-// });
