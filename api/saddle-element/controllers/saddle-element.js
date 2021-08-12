98ebf62c95d4df827712dee34864616609f3b23d
Łukasz Szarpak <lukasz.szarpak@plus.pl>
2021-08-12 12:18:49 +0200

    revert the revert

--- api/saddle-element/controllers/saddle-element.js
+++ api/saddle-element/controllers/saddle-element.js
@@ -14,31 +14,31 @@ module.exports = {
      * @param {*} ctx 
      * @returns {Array}
      */
-//   async find(ctx) {
-//     let entities;
-//     if (ctx.query._q) {
-//       entities = await strapi.services['saddle-element'].search(ctx.query);
-//     } else {
-//       entities = await strapi.services['saddle-element'].find(ctx.query);
-//     }
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services['saddle-element'].search(ctx.query);
+    } else {
+      entities = await strapi.services['saddle-element'].find(ctx.query);
+    }
 
-//     return entities.map(entity => {
-//         entity = {
-//             id: entity.id,
-//             z_index: entity.z_index,
-//             priority: entity.priority,
-//             title: entity.title,
-//             saddle_element_variants: entity.saddle_element_variants.map(variant => ({
-//                 id: variant.id,
-//                 title: variant.title,
-//                 thumbnail_hex_color: variant.thumbnail_hex_color,
-//                 title: variant.title,
-//                 img: {
-//                     url: variant.img.url
-//                 }
-//             }))
-//         }
-//         return sanitizeEntity(entity, { model: strapi.models['saddle-element'] })
-//     });
-//   },
+    return entities.map(entity => {
+        entity = {
+            id: entity.id,
+            z_index: entity.z_index,
+            priority: entity.priority,
+            title: entity.title,
+            saddle_element_variants: entity.saddle_element_variants.map(variant => ({
+                id: variant.id,
+                title: variant.title,
+                thumbnail_hex_color: variant.thumbnail_hex_color,
+                title: variant.title,
+                img: {
+                    url: variant.img.url
+                }
+            }))
+        }
+        return sanitizeEntity(entity, { model: strapi.models['saddle-element'] })
+    });
+  },
 };
