a54536784a018dbc05d17b42da5a1942e3ecc8e7
Łukasz Szarpak <lukasz.szarpak@plus.pl>
2021-08-12 11:40:01 +0200

    customize resposnse data in saddle-type and saddle-element

--- api/saddle-element/controllers/saddle-element.js
+++ api/saddle-element/controllers/saddle-element.js
@@ -5,4 +5,38 @@
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+    /**
+     * CUSTOM ENDPOINT CUSTOMIZATION
+     * 
+     * @param {*} ctx 
+     * @returns {Array}
+     */
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services['saddle-element'].search(ctx.query);
+    } else {
+      entities = await strapi.services['saddle-element'].find(ctx.query);
+    }
+
+    return entities.map(entity => {
+        entity = {
+            z_index: entity.z_index,
+            priority: entity.priority,
+            title: entity.title,
+            saddle_element_variants: entity.saddle_element_variants.map(variant => ({
+                title: variant.title,
+                thumbnail_hex_color: variant.thumbnail_hex_color,
+                title: variant.title,
+                img: {
+                    url: variant.img.url
+                }
+            }))
+        }
+        return sanitizeEntity(entity, { model: strapi.models['saddle-element'] })
+    });
+  },
+};
--- api/saddle-type/controllers/saddle-type.js
+++ api/saddle-type/controllers/saddle-type.js
@@ -5,4 +5,36 @@
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+    /**
+     * CUSTOM ENDPOINT CUSTOMIZATION
+     * 
+     * @param {*} ctx 
+     * @returns {Array}
+     */
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services['saddle-type'].search(ctx.query);
+    } else {
+      entities = await strapi.services['saddle-type'].find(ctx.query);
+    }
+
+    return entities.map(entity => {
+        entity = {
+            title: entity.title,
+            background_img: {
+                url: entity.background_img.url
+            },
+            saddle_elements: entity.saddle_elements.map(element => ({
+                z_index: element.z_index,
+                priority: element.priority,
+                title: element.title,
+            }))
+        }
+        return sanitizeEntity(entity, { model: strapi.models['saddle-type'] })
+    });
+  },
+};
