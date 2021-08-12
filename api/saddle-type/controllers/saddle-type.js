cf81fc9bf3a88e1828524f6b7fa7752a29ac841d
Łukasz Szarpak <lukasz.szarpak@plus.pl>
2021-08-12 12:12:56 +0200

    revert

--- api/saddle-type/controllers/saddle-type.js
+++ api/saddle-type/controllers/saddle-type.js
@@ -14,29 +14,29 @@ module.exports = {
      * @param {*} ctx 
      * @returns {Array}
      */
-  async find(ctx) {
-    let entities;
-    if (ctx.query._q) {
-      entities = await strapi.services['saddle-type'].search(ctx.query);
-    } else {
-      entities = await strapi.services['saddle-type'].find(ctx.query);
-    }
+//   async find(ctx) {
+//     let entities;
+//     if (ctx.query._q) {
+//       entities = await strapi.services['saddle-type'].search(ctx.query);
+//     } else {
+//       entities = await strapi.services['saddle-type'].find(ctx.query);
+//     }
 
-    return entities.map(entity => {
-        entity = {
-            id: entity.id,
-            title: entity.title,
-            background_img: {
-                url: entity.background_img.url
-            },
-            saddle_elements: entity.saddle_elements.map(element => ({
-                id: element.id,
-                z_index: element.z_index,
-                priority: element.priority,
-                title: element.title,
-            }))
-        }
-        return sanitizeEntity(entity, { model: strapi.models['saddle-type'] })
-    });
-  },
+//     return entities.map(entity => {
+//         entity = {
+//             id: entity.id,
+//             title: entity.title,
+//             background_img: {
+//                 url: entity.background_img.url
+//             },
+//             saddle_elements: entity.saddle_elements.map(element => ({
+//                 id: element.id,
+//                 z_index: element.z_index,
+//                 priority: element.priority,
+//                 title: element.title,
+//             }))
+//         }
+//         return sanitizeEntity(entity, { model: strapi.models['saddle-type'] })
+//     });
+//   },
 };
